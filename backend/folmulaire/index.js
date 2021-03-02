81cfabb659925cdc88ac8cbddf0f75deef174e1d
Ewa <kalistaewa@gmail.com>
2021-03-02 14:35:03 +0100

    folmulaire works

--- backend/folmulaire/index.js
+++ backend/folmulaire/index.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressHandlebars = require("express-handlebars");
+
+app.engine("handlebars", expressHandlebars());
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log("Server started on port " + port);
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/page", (req, res) => {
+  res.render("page");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.post("/home", (req, res) => {
+  console.log(req.body);
+  res.redirect("/page");
 });
