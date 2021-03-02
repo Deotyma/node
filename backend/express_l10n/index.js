d08d069db3e6cc6adb8589dd476fc99679a810cd
Ewa <kalistaewa@gmail.com>
2021-03-02 17:01:32 +0100

    translations with switch

--- backend/express_l10n/index.js
+++ backend/express_l10n/index.js
@@ -10,3 +10,19 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log("Server started on port " + port);
 });
+
+app.get("/:lang?", (req, res) => {
+  switch (req.params.lang) {
+    case "en":
+      res.render("home", { title: "Hello, how are you" });
+      break;
+    case "fr":
+      res.render("home", { title: "Bonjour, ça va?" });
+      break;
+    case "es":
+      res.render("home", { title: "Hola que tal?" });
+      break;
+    default:
+      res.render("home", { title: "Bonjour, ça va?" });
+  }
+});
