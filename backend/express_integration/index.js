40fe65500ac252e6d3c0c12cfbd60123653eb68b
Ewa <kalistaewa@gmail.com>
2021-03-03 17:29:31 +0100

    active in navBar

--- backend/express_integration/index.js
+++ backend/express_integration/index.js
@@ -12,29 +12,49 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.render("home", { css: "/css/homepage.css" });
+  res.render("home", {
+    css: "/css/homepage.css",
+    title: "Cui'zine Acceuil",
+    activeHome: true,
+  });
 });
 
 app.get("/equipe", (req, res) => {
-  res.render("team", { css: "/css/team.css" });
+  res.render("team", {
+    css: "/css/team.css",
+    title: "équipe",
+    activeEquipe: true,
+  });
 });
 
 app.get("/equipe-membre", (req, res) => {
-  res.render("team-membre", { css: "/css/team-member.css" });
+  res.render("team-membre", {
+    css: "/css/team-member.css",
+    title: "membre",
+    activeEquipe: true,
+  });
 });
 
 app.get("/blog", (req, res) => {
-  res.render("recipe", { css: "/css/recipe.css" });
+  res.render("recipe", {
+    css: "/css/recipe.css",
+    title: "blog",
+    activeBlog: true,
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", { css: "/css/contact.css" });
+  res.render("contact", {
+    css: "/css/contact.css",
+    title: "contact",
+    activeContact: true,
+  });
 });
 
 app.get("/post", (req, res) => {
-  res.render("post", { css: "/css/post.css" });
+  res.render("post", { css: "/css/post.css", title: "post", active: true });
 });
 
 app.get("/page2", (req, res) => {
-  res.render("page2", { css: "/css/homepage.css" });
+  res.render("page2", { css: "/css/homepage.css", title: "page2" });
 });
